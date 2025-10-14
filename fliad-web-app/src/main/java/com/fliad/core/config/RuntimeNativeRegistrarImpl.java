9fdadd319faf572f4111823bcb90efa8f9709969
wyl <791128058@qq.com>
2025-10-14 10:08:38 +0800

    aot编译支持

---
+++ fliad-web-app/src/main/java/com/fliad/core/config/RuntimeNativeRegistrarImpl.java
@@ -0,0 +1,18 @@
+package com.fliad.core.config;
+
+import com.github.xiaoymin.knife4j.solon.settings.OpenApiSetting;
+import org.noear.solon.annotation.Component;
+import org.noear.solon.aot.RuntimeNativeMetadata;
+import org.noear.solon.aot.RuntimeNativeRegistrar;
+import org.noear.solon.aot.hint.MemberCategory;
+import org.noear.solon.core.AppContext;
+
+@Component
+public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
+
+    @Override
+    public void register(AppContext context, RuntimeNativeMetadata metadata) {
+        metadata.registerReflection(OpenApiSetting.class,  MemberCategory.values());
+    }
+
+}
