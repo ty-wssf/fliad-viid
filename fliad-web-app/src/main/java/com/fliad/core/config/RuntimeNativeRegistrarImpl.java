f57e7abb05efc2950a38a57edb6c632fdff18391
wyl <791128058@qq.com>
2025-10-15 09:05:32 +0800

    aot编译支持

--- fliad-web-app/src/main/java/com/fliad/core/config/RuntimeNativeRegistrarImpl.java
+++ fliad-web-app/src/main/java/com/fliad/core/config/RuntimeNativeRegistrarImpl.java
@@ -33,6 +33,16 @@ public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
         metadata.registerReflection(FlexGlobalConfig.KeyConfig.class,  MemberCategory.values());
         metadata.registerReflection(FastRuntimeEngine.class,  MemberCategory.values());
         metadata.registerReflection(EntitySqlProvider.class,  MemberCategory.values());
+
+        // 允许访问 java.lang.invoke 包中的内部API，解决反射调用相关问题
+        metadata.registerArg("-J--add-opens=java.base/java.lang.invoke=ALL-UNNAMED");
+        // 允许访问 java.nio 包中的内部API，通常用于直接内存访问或缓冲区操作
+        metadata.registerArg("-J--add-opens=java.base/java.nio=ALL-UNNAMED");
+        // 导出 jdk.internal.misc 包，使其对所有模块可见，用于访问Unsafe等内部工具类
+        metadata.registerArg("-J--add-exports=java.base/jdk.internal.misc=ALL-UNNAMED");
+
+        // 设置生成的native镜像的目标CPU架构兼容性模式
+        metadata.registerArg("-march=compatibility");
     }
 
 }
