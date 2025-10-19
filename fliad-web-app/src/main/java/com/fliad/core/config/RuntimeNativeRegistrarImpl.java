feaa6a3f7d9c2745d51c54e3db9fafa0bed6843e
吴益龙 <79128058@qq.com>
2025-10-19 10:43:57 +0800

    升级脚本为jdk25

--- fliad-web-app/src/main/java/com/fliad/core/config/RuntimeNativeRegistrarImpl.java
+++ fliad-web-app/src/main/java/com/fliad/core/config/RuntimeNativeRegistrarImpl.java
@@ -83,7 +83,6 @@ public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
         metadata.registerArg("-H:IncludeResourceBundles=org.graalvm.js.resources");
         // # 允许部分类路径不完整（JS 引擎可能依赖动态加载类）
         metadata.registerArg("--allow-incomplete-classpath");
-        metadata.registerArg("--features=org.graalvm.js.JSFeature");
 
     }
 
