450e20239562dd896b20b83db988fced39de214b
吴益龙 <79128058@qq.com>
2025-10-24 21:21:31 +0800

    移除lombok

--- fliad-web-app/src/main/java/com/fliad/core/config/RuntimeNativeRegistrarImpl.java
+++ fliad-web-app/src/main/java/com/fliad/core/config/RuntimeNativeRegistrarImpl.java
@@ -71,6 +71,7 @@ public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
         metadata.registerReflection(org.apache.xmlbeans.impl.store.Locale.class, MemberCategory.values());
 
         metadata.registerReflection(ch.qos.logback.classic.Logger.class, MemberCategory.values());
+        metadata.registerReflection(gov.nist.core.LogWriter.class, MemberCategory.values());
 
         metadata.registerJdkProxy(HCNetSDK.class);
         metadata.registerJdkProxy(HttpUtils.class);
