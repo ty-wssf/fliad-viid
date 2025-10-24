8bb13407a58688319804d349b5d730e56c1e4910
吴益龙 <79128058@qq.com>
2025-10-24 23:23:09 +0800

    sip支持native环境

--- fliad-plugin/fliad-plugin-sip-gb28181/src/main/java/com/fliad/sip/gb28181/core/config/RuntimeNativeRegistrarImpl.java
+++ fliad-plugin/fliad-plugin-sip-gb28181/src/main/java/com/fliad/sip/gb28181/core/config/RuntimeNativeRegistrarImpl.java
@@ -6,6 +6,8 @@ import org.noear.solon.aot.RuntimeNativeMetadata;
 import org.noear.solon.aot.RuntimeNativeRegistrar;
 import org.noear.solon.aot.hint.MemberCategory;
 import org.noear.solon.core.AppContext;
+import org.noear.solon.core.util.ClassUtil;
+import org.noear.solon.core.util.ScanUtil;
 
 @Component
 public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
@@ -15,6 +17,15 @@ public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
         metadata.registerReflection(SipStackImpl.class, MemberCategory.values());
         metadata.registerReflection(gov.nist.core.LogWriter.class, MemberCategory.values());
         metadata.registerReflection(gov.nist.javax.sip.stack.ServerLog.class, MemberCategory.values());
+        metadata.registerReflection(gov.nist.javax.sip.stack.DefaultRouter.class, MemberCategory.values());
+        ScanUtil.scan(context.getClassLoader(), "gov/nist", n -> n.endsWith(".class")).forEach(name -> {
+            String className = name.substring(0, name.length() - 6);
+            className = className.replace('/', '.');
+            Class<?> clz = ClassUtil.loadClass(context.getClassLoader(), className);
+            if (clz != null && !clz.isInterface() && !clz.isAnnotation() && !clz.isEnum() && !clz.isSynthetic() && !clz.isArray() && !clz.isPrimitive()) {
+                metadata.registerReflection(clz, MemberCategory.values());
+            }
+        });
     }
 
 }
