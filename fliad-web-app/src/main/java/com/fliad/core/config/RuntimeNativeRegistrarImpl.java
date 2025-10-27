73953ac99ab37b64beb77b72a4cb686141f5bbe6
wyl <791128058@qq.com>
2025-10-27 10:07:37 +0800

    缓存前端构建产物2

--- fliad-web-app/src/main/java/com/fliad/core/config/RuntimeNativeRegistrarImpl.java
+++ fliad-web-app/src/main/java/com/fliad/core/config/RuntimeNativeRegistrarImpl.java
@@ -91,6 +91,7 @@ public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
 
         metadata.registerResourceInclude("_sql/.*");
         metadata.registerResourceInclude("app-local.yml");
+        metadata.registerResourceInclude("app-local.yml");
 
         // 允许访问 java.lang.invoke 包中的内部API，解决反射调用相关问题
         metadata.registerArg("-J--add-opens=java.base/java.lang.invoke=ALL-UNNAMED");
