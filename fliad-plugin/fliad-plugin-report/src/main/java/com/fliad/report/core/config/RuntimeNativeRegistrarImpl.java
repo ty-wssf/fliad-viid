7d0052e5e28327c6af1ebec2171876c635d6170c
wyl <791128058@qq.com>
2025-10-28 11:06:04 +0800

    报表功能集成

---
+++ fliad-plugin/fliad-plugin-report/src/main/java/com/fliad/report/core/config/RuntimeNativeRegistrarImpl.java
@@ -0,0 +1,19 @@
+package com.fliad.report.core.config;
+
+import io.nop.report.core.build.XptModelLoader;
+import org.noear.solon.annotation.Component;
+import org.noear.solon.aot.RuntimeNativeMetadata;
+import org.noear.solon.aot.RuntimeNativeRegistrar;
+import org.noear.solon.aot.hint.MemberCategory;
+import org.noear.solon.core.AppContext;
+
+
+@Component
+public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
+
+    @Override
+    public void register(AppContext context, RuntimeNativeMetadata metadata) {
+        metadata.registerReflection(XptModelLoader.class, MemberCategory.values());
+    }
+
+}
