fb14e00f44a9a9958782a28ddfd602d43ee59d20
吴益龙 <79128058@qq.com>
2025-11-09 09:36:45 +0800

    antlr4-tool模块提交

---
+++ fliad-plugin/fliad-plugin-nop/fliad-plugin-nop-antlr4-tool/src/main/java/io/nop/antlr4/tool/model/StarBlock.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2017-2024 Nop Platform. All rights reserved.
+ * Author: canonical_entropy@163.com
+ * Blog:   https://www.zhihu.com/people/canonical-entropy
+ * Gitee:  https://gitee.com/canonical-entropy/nop-entropy
+ * Github: https://github.com/entropy-cloud/nop-entropy
+ */
+package io.nop.antlr4.tool.model;
+
+public class StarBlock extends GrammarElement {
+
+    @Override
+    public GrammarElementKind getKind() {
+        return GrammarElementKind.STAR_BLOCK;
+    }
+
+}
